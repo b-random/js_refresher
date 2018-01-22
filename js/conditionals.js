83617eff0f12f7bc17c29f545992bd98fcf217a9
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-22 16:53:12 -0600

    removed redundancy after last commit

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,7 +1,7 @@
 var age = prompt("What is your age?");
 if(age < 18){
 	console.log("Sorry youngin\', but you are too young to get in!");
-} else if(age > 18 && age < 21){
+} else if(age < 21){
 	console.log("You can get in the club, but no drinking!");
 } else {
 	console.log("Have a good time!");
