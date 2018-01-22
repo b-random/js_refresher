a4690637374f3b50da6ef5cffbcd55251e256385
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-21 23:41:38 -0600

    Quick exercise with js concatenation

---
+++ js/script2.js
@@ -0,0 +1,5 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("How old are you?");
+console.log("Hello, " + first + " " + last + ".");
+console.log("you are " + age + " years old!");
