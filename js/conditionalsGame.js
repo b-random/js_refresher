559428ad0215e2c2846f1422de3223e229bba619
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-22 19:01:50 -0600

    first logic

--- js/conditionalsGame.js
+++ js/conditionalsGame.js
@@ -1,1 +1,9 @@
-alert("Testing");
+var secretNumber = 4;
+var guess = prompt("Guess a number to win!!!");
+
+if(guess == secretNumber){
+	alert("You guessed the secret number, 4! You are a winner!");
+} else {
+	alert("Womp, womp. Sorry, you are a loser.");
+	
+};
