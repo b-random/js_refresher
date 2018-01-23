2890654654e260469a4d75d7d20a56cf876a6090
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-22 18:15:29 -0600

    Conditional exercise logic

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,8 +1,18 @@
 var age = prompt("What is your age?");
-if(age < 18){
-	console.log("Sorry youngin\', but you are too young to get in!");
-} else if(age < 21){
-	console.log("You can get in the club, but no drinking!");
+//var squared = Math.sqrt(age);
+
+if(age % Math.sqrt(age) === 0){//if(Number.isInteger(squared) && squared * squared == age){
+	console.log("PERFECT SQUARE!");
+} else if(age < 18 && age % 2 === 1){
+	console.log("Your age is odd.");
+} else if(age < 18){
+	console.log("Sorry kid, but you are too young to get in!");
+} else if(age < 21 && age % 2 === 1){
+	console.log("Your age is odd.");
+} else if(age < 21){ 
+	console.log("You may enter, but you cannot drink!");
+} else if(age === 21){
+	console.log("Happy 21st Birthday!");
 } else {
 	console.log("Have a good time!");
 };
