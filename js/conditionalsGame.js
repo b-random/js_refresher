c7c276f7b471d1a1c3fe2d25ff715cef31b4d9f6
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-22 19:04:50 -0600

    Simplified logic

--- js/conditionalsGame.js
+++ js/conditionalsGame.js
@@ -1,9 +1,9 @@
 var secretNumber = 4;
 var guess = prompt("Guess a number to win!!!");
 
-if(guess == secretNumber){
+if(Number(guess) === secretNumber){
 	alert("You guessed the secret number, 4! You are a winner!");
 } else {
 	alert("Womp, womp. Sorry, you are a loser.");
-	
+
 };
