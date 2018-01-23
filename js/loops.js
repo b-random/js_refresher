002fe7d43f528d3705d522168186a7f05a64018d
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-22 20:00:23 -0600

    new logic for loops refresher

--- js/loops.js
+++ js/loops.js
@@ -1,1 +1,14 @@
-alert("test");
+//var num = 1;
+
+//while(num <= 10){
+//	console.log(num);
+//	num++;
+//};
+
+var str = "howdy";
+var count = 0;
+
+while(count < str.length) {
+	console.log(str[count]);
+	count++;
+}
