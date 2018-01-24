7db210f4c2ef45b4c5c9fdefdead3f621cf86e36
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-24 16:52:55 -0600

    Factorial function refresher

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,25 @@
-alert("test");
+//function test(x, y){
+//	console.log(y-x);
+//	return y - x;
+//};
+//
+//test(10, 40);
+
+//var isEven = function(n){
+//	if(n % 2 === 0){
+//		return true;
+//	} else {
+//		return false;
+//	};
+//};
+
+var fact = function(n){
+	if(n === 0 || n === 1){
+		return 1;
+	};
+	for(var i = n -1; i >= 1; i--){
+		n = n * i;
+	};
+  	return n;
+};
+
