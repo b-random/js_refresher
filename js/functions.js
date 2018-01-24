057819cdc3b87dc889da15bc43291fd08bf6cb13
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-24 16:59:59 -0600

    Character replace function

--- js/functions.js
+++ js/functions.js
@@ -13,13 +13,18 @@
 //	};
 //};
 
-var fact = function(n){
-	if(n === 0 || n === 1){
-		return 1;
-	};
-	for(var i = n -1; i >= 1; i--){
-		n = n * i;
-	};
-  	return n;
+//var fact = function(n){
+//	if(n === 0 || n === 1){
+//		return 1;
+//	};
+//	for(var i = n -1; i >= 1; i--){
+//		n = n * i;
+//	};
+//  	return n;
+//};
+
+function SnakeToKebab(str) {
+	var newStr = str.replace(/_/g, "-");
+	return newStr;
 };
 
