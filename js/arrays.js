0003a8a36900a7fdec8181978b2303351b82b8cf
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-25 11:51:54 -0600

    Push/pop refresher

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,14 @@
-alert("test");
+//var arr = ["red", "white", "blue"];
+//function printer(x){
+//	console.log(arr[x]);
+//};
+
+//printer(1);// white
+
+var arr = ["red", "white", "blue"];
+
+arr.push("green");
+
+arr.pop();
+
+
