c28d5728ae72b19d2779a61fdc17074e70c9d928
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-27 20:46:37 -0600

    Another way to create objects

--- js/objects.js
+++ js/objects.js
@@ -7,12 +7,24 @@ var person1 = {
 };
 
 //two ways to call
-console.log(person1.name);
-console.log(person1["age"]);
-
-var str = "city";
+//console.log(person1.name);
+//
+//var str = "city";
 
 //if key is set to a variable, it must be called with brackets
 
 
-console.log(person1[str]);
+//console.log(person1[str]);
+
+//reassign a keys value
+
+//person1.name = "John";
+
+//console.log(person1);
+
+var person2 = new Object();
+
+person2.job = "Tech";
+person2.weight = 190;
+
+console.log(person2);
