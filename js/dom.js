7ce40d3561790d87dcca2c9bba3470ffe5d0546d
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-29 16:38:15 -0600

    Manipulating the Dom with querySelector

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,15 @@
-alert("test");
+var h1 = document.querySelector("h1");
+
+var body = document.querySelector("body");
+var isBlue = false;
+
+setInterval(function(){
+  if(isBlue){
+  	body.style.background = "white";
+  	h1.style.color = "red";
+  } else {
+  	body.style.background = "#3498db"
+  	h1.style.color = "orange";
+  }
+  isBlue = !isBlue;
+}, 1000);
