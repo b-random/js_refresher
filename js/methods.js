4e04faf3e03ea0352d7025cff52d15dd30ab0569
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-29 14:20:44 -0600

    Like-named methods within objects refresher

--- js/methods.js
+++ js/methods.js
@@ -1,15 +1,45 @@
-var myObj = {
-	name: "Bob",
-	age: 35,
-	isCool: false,
-	friends: ["jim", "Jerry"], 
-	add: function(x, y){
-		return x + y;
+//var myObj = {
+//	name: "Bob",
+//	age: 35,
+//	isCool: false,
+//	friends: ["jim", "Jerry"], 
+//	add: function(x, y){
+//		return x + y;
+//	}
+//};
+
+//myObj.add(5, 5);
+//var a = myObj.add(5, 5);
+
+//a + 10;
+//20
+
+//methods allow you to group create functions of the same name at different areas of an object
+
+var dog = {
+	species: "dog",
+	name: "Spot",
+
+	says: function(){
+		console.log("WOOF!");
 	}
 };
 
-//myObj.add(5, 5);
-var a = myObj.add(5, 5);
+var cat = {
+	species: "cat",
+	name: "Tabby",
+	says: function(){
+		console.log("MEOW!");
+	}
+};
 
-a + 10;
-//20
+dog.says();
+cat.says();
+
+var bird = {};
+
+bird.says = function(){
+	console.log("TWEET-TWEET!");
+};
+
+bird.says();
