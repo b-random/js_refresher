bc662e2a991b0177d3dbc2fbdbdc1b69cf510351
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-29 12:01:51 -0600

    Using a method within an object

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,15 @@
-alert("test");
+var myObj = {
+	name: "Bob",
+	age: 35,
+	isCool: false,
+	friends: ["jim", "Jerry"], 
+	add: function(x, y){
+		return x + y;
+	}
+};
+
+//myObj.add(5, 5);
+var a = myObj.add(5, 5);
+
+a + 10;
+//20
