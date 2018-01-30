11dd3b707cd16c8ccc47cbc2d71c0ca51943c1f4
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-01-29 19:01:51 -0600

    Creating a function to iterate through selector returned arrays

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,44 @@
-var h1 = document.querySelector("h1");
+//var h1 = document.querySelector("h1");
 
-var body = document.querySelector("body");
-var isBlue = false;
+//var body = document.querySelector("body");
+//var isBlue = false;
 
-setInterval(function(){
-  if(isBlue){
-  	body.style.background = "white";
-  	h1.style.color = "red";
-  } else {
-  	body.style.background = "#3498db"
-  	h1.style.color = "orange";
-  }
-  isBlue = !isBlue;
-}, 1000);
+//setInterval(function(){
+//  if(isBlue){
+//  	body.style.background = "white";
+//  	h1.style.color = "red";
+//  } else {
+//  	body.style.background = "#3498db"
+//  	h1.style.color = "orange";
+//  }
+//  isBlue = !isBlue;
+//}, 1000);
+
+//Dom selector methods...
+
+//.getElementById
+var tag = document.getElementById("highlight");
+tag.style.color = "orange";
+
+//.getElementsByClassName
+var tags = document.getElementsByClassName("bolded");
+tags[0].style.color = "red"; //ByClassName returns an array and the elements must be referred to by position
+
+//.getElementsByTagName
+var elTag = document.getElementsByTagName("li");
+elTag[1].style.fontWeight = 800;
+
+//iterate through all elements returned by TagName^
+function thruElTag(arr){
+	for(var i = 0; i < arr.length; i++ ){
+		arr[i].style.color = "pink";
+	};
+};
+
+thruElTag(elTag);
+
+//.querySelector
+//returns first of kind
+
+var first = document.querySelector("h1");
+first.style.color = "red";
