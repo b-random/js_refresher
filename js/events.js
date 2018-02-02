7f3f865bcfe4d967eab2e0b583ba36d0e05bc837
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-02 15:34:14 -0600

    Event listeners for child elements, one at a time

--- js/events.js
+++ js/events.js
@@ -1,7 +1,16 @@
-var hh = document.querySelector("h1");
+//var hh = document.querySelector("h1");//
 
-hh.addEventListener("click", function(){
-  hh.textContent = "Howdy!";
-  hh.style.background = "orange"
-});
+//hh.addEventListener("click", function(){
+//  hh.textContent = "Howdy!";
+//  hh.style.background = "orange"
+//});
 
+//how to select and creat an event on specific, buried elements
+var li = document.querySelectorAll("li");
+
+for(var i = 0; i < li.length; i++){
+	li[i].addEventListener("click", function(){
+		this.style.color = "red";
+	})
+}
+//adds event on click to each li individually 
