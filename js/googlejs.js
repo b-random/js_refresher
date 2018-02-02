aa1542162e46a078d0a145e61ce13c640062a7d4
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-01 23:43:05 -0600

    manipulating the DOM

--- js/googlejs.js
+++ js/googlejs.js
@@ -1,1 +1,14 @@
-alert("test");
+//find all links
+
+var links = document.getElementsByTag("a");
+
+for(var i - 0; i < links.length; i++){
+	console.log(links[i].getAttribute("href"));
+};
+//loops through and prints all links
+
+//set all links to www.somesite.com
+
+for(var i - 0; i < links.length; i++){
+	links[i].setAttribute("href", "http://www.somesite.com");
+};
