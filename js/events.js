977d90e0cb4e4094f020458c330ebe68ccad392e
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-02 15:39:28 -0600

    Add event listener with anon function

--- js/events.js
+++ js/events.js
@@ -6,11 +6,23 @@
 //});
 
 //how to select and creat an event on specific, buried elements
+//var li = document.querySelectorAll("li");
+
+//for(var i = 0; i < li.length; i++){
+//	li[i].addEventListener("click", function(){
+//		this.style.color = "red";
+//	})
+//}
+//adds event on click to each li individually
+
+ //do the same thing, but without using an annonymous function
+
 var li = document.querySelectorAll("li");
 
+var change = function(){
+	this.style.color = "yellow"
+};
+
 for(var i = 0; i < li.length; i++){
-	li[i].addEventListener("click", function(){
-		this.style.color = "red";
-	})
-}
-//adds event on click to each li individually 
+	li[i].addEventListener("click", change);
+};
