319d55f8163112d6cd4df4d55d3615c92701604b
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-05 18:00:28 -0600

    Enabling CSS event listener

--- js/moreEvents.js
+++ js/moreEvents.js
@@ -1,9 +1,23 @@
-var first = document.querySelector("li");
+var first = document.querySelectorAll("li");
 
-first.addEventListener("mouseover", function(){
-	first.style.color = "red";
-});
+//first.addEventListener("mouseover", function(){
+//	first.style.color = "red";
+//});//
 
-first.addEventListener("mouseout", function(){
-	first.style.color = "black";
-});
+//first.addEventListener("mouseout", function(){
+//	first.style.color = "black";
+//});
+
+for(var i = 0; i < first.length; i++){
+	first[i].addEventListener("mouseover", function(){
+	  this.style.color = "red";
+    });
+
+    first[i].addEventListener("mouseout", function(){
+	  this.style.color = "black";
+    });
+
+    first[i].addEventListener("click", function(){
+    	this.classList.toggle("done");
+    });
+};
