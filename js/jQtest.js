0ec5fabb5fc265919a183357de2dad8eb2e1805f
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-21 16:02:03 -0600

    jQ test objectives

--- js/jQtest.js
+++ js/jQtest.js
@@ -1,1 +1,6 @@
-alert('test');
+// reference jQuery
+// Give all divs a purple background
+// all divs with class highlight are 200px wide
+// all divs with id third have orange border
+// Select the first div and change the font to pink
+
