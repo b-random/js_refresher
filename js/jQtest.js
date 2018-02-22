d65f4771a606e25be63a882181e1dcbfa7be0bee
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-22 11:58:34 -0600

    Using nth-of-type with jQ

--- js/jQtest.js
+++ js/jQtest.js
@@ -7,5 +7,5 @@
 $('div').css('background', 'purple');
 $('.highlight').css('width', '200px');
 $('#third').css('border', '2px solid orange');
-$('div:first').css('color', 'pink');
+$('div:nth-of-type(2)').css('color', 'pink');
 
