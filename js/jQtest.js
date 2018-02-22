30ffb44eebdbdd58ee452a02425b306d746245ce
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-02-22 11:49:26 -0600

    jQ test completed

--- js/jQtest.js
+++ js/jQtest.js
@@ -4,3 +4,8 @@
 // all divs with id third have orange border
 // Select the first div and change the font to pink
 
+$('div').css('background', 'purple');
+$('.highlight').css('width', '200px');
+$('#third').css('border', '2px solid orange');
+$('div:first').css('color', 'pink');
+
