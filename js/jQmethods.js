4a4bfd92cdba67ee3f73a6c19160d18b15cef7d1
Brandon Scott Brigance <brandon.brigance@gmail.com>
2018-03-01 11:50:06 -0600

    jQ attr() method

--- js/jQmethods.js
+++ js/jQmethods.js
@@ -16,3 +16,18 @@ $('li').text("abcdefg");
 
 // .html()
 $('li:first-of-type').html("<a href='google.com'>Google!</a>");
+
+
+
+//.attr() method for changing attributes
+
+//console.log($("img").css("width")); //find width
+
+$("img").css("width", "300px")
+$("img").css("height", "175px")
+
+$("img").attr("src", "https://cmeimg-a.akamaihd.net/640/photos.demandstudios.com/getty/article/3/40/153128077.jpg");
+
+console.log($("input").attr("type"));
+
+$("input").attr("type", "checkbox");
